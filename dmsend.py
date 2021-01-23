ab8ebbf08177acd6ebbda504a76fd587ae55e391
Gangmin0923 <77842905+Gangmin0923@users.noreply.github.com>
2021-01-23 16:31:10 +0900

    Update dmsend.py

--- dmsend.py
+++ dmsend.py
@@ -12,7 +12,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("봇실행이 시작되었습니다(24시간 온라인).")
-    game = discord.Game('~하는 중 ex)봇만들기 하는 중')
+    game = discord.Game('테스트')
     await client.change_presence(status=discord.Status.online, activity=game)
 
 #/dm {할말}로 전체DM 전송
@@ -25,7 +25,7 @@ async def on_message(message):
             else:
                 try:
                     msg = message.content[4:]
-                    if message.author.id == 디스코드ID를 적기!!:
+                    if message.author.id == 575325361458642944:
                         embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="최상단 제목")
                         embed.add_field(name="제목", value=msg, inline=True)
                         embed.set_footer(text=f"서버초대코드")
@@ -34,4 +34,4 @@ async def on_message(message):
                     pass
 
 
-client.run('봇의 토큰을 넣어주세용')
+client.run('Nzk5MzA0MzM4MDk0MDMwODQ5.YABoLA.R5Bnw9Xvn1KcH3ZymNqJf5B1PmQ')
